c33c59730513a3e80a6c45fa2635bd24c2582424
JamieDawson <jamierobertdawson@gmail.com>
2020-08-22 17:13:08 -0700

    stopped at 17:40. isAuthenticated capabilites to confirm if we are logged in

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import LoginButton from './components/LoginButton';
 import LogoutButtom from './components/LogoutButton';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <>
       <LoginButton></LoginButton>
       <LogoutButtom></LogoutButtom>
+      <Profile></Profile>
     </>
   );
 }
--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {useAuth0} from '@auth0/auth0-react';
 
 const LogoutButton = () => {
-  const {logout} = useAuth0();
-  return <button onClick={() => logout()}>logout</button>;
+  const {logout, isAuthenticated} = useAuth0();
+
+  return !isAuthenticated && <button onClick={() => logout()}>logout</button>;
 };
 
 export default LogoutButton;
